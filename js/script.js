a3f546852c858fc81d66c081f5082093510a8161
Jesse McKenzie <jfmckenz31@gmail.com>
2018-06-29 00:45:50 -0400

    added background-color gradient, rounded social media and button link borders, set top/bottom 15px padding to header on md viewports to

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@
 //     .slideDown(1000)
 //     .delay(3000)
 //     .slideUp();
+
+// $(function () {
+//     $('a.hamburger').click(function () {
+//         toggleNavbar();
+//     });
+// });
+
+// function toggleNavbar() {
+//     $('.nav-btn-group').toggleClass('navbarOpen');
+// }
+$(document).ready(function () {
+    $('span.mobile').click(function () {
+        $('#navbar').toggle();
+    });
+});
